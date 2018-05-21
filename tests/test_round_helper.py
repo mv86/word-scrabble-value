cf4e215fffc80e291f9e90c24ff1149bc752be3b
Max Veasey <mveasey87@gmail.com>
2018-05-21 20:12:40 +0100

    finished unit tests for round_helper

--- tests/test_round_helper.py
+++ tests/test_round_helper.py
@@ -4,22 +4,11 @@ from unittest.mock import patch
 import pytest
 
 from player import Player
-from round_helper import _is_valid_word, _prompt_for_word, _word_in_hand
+from round_helper import (_get_optimal_words, _is_valid_word, _prompt_for_word,
+                          _remove_tiles, _word_in_hand)
 
 
-HAND = ['Z', 'O', 'E', 'T', 'A', 'O', 'P']
-
-@pytest.mark.parametrize('word, hand, rt_val', [
-    ('at', [], False),
-    ('', HAND, False),
-    ('at', HAND, True),
-    ('poet', HAND, True), # Handles duplicates in hand (P)
-    ('zoo', HAND, True),  # Handles duplicates in word (O)
-    ('trap', HAND, False),  # No 'R'
-    ('tote', HAND, False)  # Double use of 'T'
-])
-def test_word_in_hand(word, hand, rt_val):
-    assert _word_in_hand(word, hand) == rt_val
+HAND = 'Z O E T A O P'.split()
 
 
 @patch('builtins.input', side_effect=['pot', 'zzz', 'no', 'top'])
@@ -42,3 +31,38 @@ def test_is_valid_word(capsys):
     assert not _is_valid_word('no', HAND)  # Not in hand
     out, _ = capsys.readouterr()
     assert out.strip() == msg
+
+
+@pytest.mark.parametrize('word, hand, rt_val', [
+    ('at', [], False),
+    ('', HAND, False),
+    ('at', HAND, True),
+    ('poet', HAND, True), # Handles duplicates in hand (O)
+    ('zoo', HAND, True),  # Handles duplicates in word (O)
+    ('trap', HAND, False),  # No 'R'
+    ('tote', HAND, False)  # Double use of 'T'
+])
+def test_word_in_hand(word, hand, rt_val):
+    assert _word_in_hand(word, hand) == rt_val
+
+
+def test_remove_tiles():
+    hand = 'Z O E T A O P'.split()
+
+    _remove_tiles('at', hand)
+    assert hand == ['Z', 'O', 'E', 'O', 'P']
+
+    _remove_tiles('zoo', hand)  # Handles duplicated letters
+    assert hand == ['E', 'P']
+
+
+def test_get_optimal_words():
+    optimal_word = _get_optimal_words(HAND)
+    assert optimal_word[0].word == 'TOPAZ'
+    assert optimal_word[0].value == 16
+
+    # Return multiple word when applicable
+    optimal_words = _get_optimal_words('A B C D E F G'.split())
+    for word, value in optimal_words:
+        assert word in ('FACED', 'DECAF')
+        assert value == 11
